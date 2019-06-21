b5f664d3d0cfb60ebc2582393b780e4698d810c0
Himanshu Bansal <himanshu.bansal@softwaysolutions.com>
2019-06-21 13:15:16 +0530

    completed chalenge 2

--- challenge2/main.js
+++ challenge2/main.js
@@ -2,43 +2,50 @@ function randomNumberGenerator(max, min = 0) {
     return Math.round(Math.random() * (max - min)) + min;
 }
 
-const width = randomNumberGenerator(30, 10);
-const height = randomNumberGenerator(20, 5);
 const containerElement = document.getElementById('container');
-
-colorApi({
-    width: width,
-    height: height,
-    count: width * height
-}).then((colors) => {
-    while (containerElement.hasChildNodes()) {
-        containerElement.removeChild(containerElement.firstChild);
-    }
-    containerElement.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
-    containerElement.style.gridTemplateRows = `repeat(${height}, 1fr)`;
-    const elements = [];
-    for (let i = 0; i < width * height; i++) {
-        const divElement = document.createElement('div');
-        divElement.classList.add('cell');
-        elements.push(divElement);
-        containerElement.appendChild(divElement);
-    }
-    const unassignedColors = [];
-    colors.forEach((color) => {
-        const place = width * color.coordinates.y + color.coordinates.x;
-        if (!elements[place].style.backgroundColor) {
-            elements[place].style.backgroundColor = color.value;
-        } else {
-            unassignedColors.push(color);
-        }
-    });
-    elements.forEach((e) => {
-        if (!e.style.backgroundColor) {
-            let colorIndex = randomNumberGenerator(unassignedColors.length - 1);
-            e.style.backgroundColor = unassignedColors.splice(colorIndex, 1)[0].value;
-        }
-    });
+document.getElementById('reRender').addEventListener('click', () => {
+    let width = parseInt(document.getElementById('widthInput').value) || randomNumberGenerator(50, 10);
+    let height = parseInt(document.getElementById('heightInput').value) || randomNumberGenerator(20, 5);
+    if (width > 50) width = 50;
+    if (height > 20) height = 20;
+    render(width, height);
 });
+
+function render(width, height) {
+    colorApi({
+        width: width,
+        height: height,
+        count: width * height
+    }).then((colors) => {
+        while (containerElement.hasChildNodes()) {
+            containerElement.removeChild(containerElement.firstChild);
+        }
+        containerElement.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+        containerElement.style.gridTemplateRows = `repeat(${height}, 1fr)`;
+        const elements = [];
+        for (let i = 0; i < width * height; i++) {
+            const divElement = document.createElement('div');
+            divElement.classList.add('cell');
+            elements.push(divElement);
+            containerElement.appendChild(divElement);
+        }
+        const unassignedColors = [];
+        colors.forEach((color) => {
+            const place = width * color.coordinates.y + color.coordinates.x;
+            if (!elements[place].style.backgroundColor) {
+                elements[place].style.backgroundColor = color.value;
+            } else {
+                unassignedColors.push(color);
+            }
+        });
+        elements.forEach((e) => {
+            if (!e.style.backgroundColor) {
+                let colorIndex = randomNumberGenerator(unassignedColors.length - 1);
+                e.style.backgroundColor = unassignedColors.splice(colorIndex, 1)[0].value;
+            }
+        });
+    });
+}
 
 function colorApi(options = {
     width: null,
@@ -62,3 +69,5 @@ function colorApi(options = {
             return response.json().then(r => r.colors);
         });
 }
+
+render(randomNumberGenerator(50, 10), randomNumberGenerator(20, 5));
